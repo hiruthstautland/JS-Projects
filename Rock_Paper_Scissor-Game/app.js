261ac829c374bb593a0bed92f5142752eb8601cd
unknown <hiruthstautland@gmail.com>
2019-08-01 03:41:35 +0200

    javascript added

--- Rock_Paper_Scissor-Game/app.js
+++ Rock_Paper_Scissor-Game/app.js
@@ -10,7 +10,8 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissor_div = document.getElementById("scissor");
-
+const user_choice = document.getElementById("user-choice");
+const computer_choice = document.getElementById("computer-choice");
 //Game rules, how the game should work
 function gameComputer() {
 	const choices = [ "rock", "paper", "scissor" ];
@@ -20,17 +21,19 @@ function gameComputer() {
 function win(userSelected, computerSelected) {
 	userScore++;
 	userScore_span.innerHTML = userScore;
-	computerScore_span.innerHTML = computerScore;
-	const smallWord = "You Win".fontsize(5).sup();
-	result_p.innerHTML = `${userSelected}(user) beats ${computerSelected}(comp). ${smallWord}`;
+	const smallWord = "You Won!".fontsize(3).sup();
+	result_p.innerHTML = `${userSelected} beats ${computerSelected}! ${smallWord}`;
 	//fix (user) and (comp) todo:
 }
-function lose() {
-	console.log("lose");
-	// computerScore_span
+function lose(userSelected, computerSelected) {
+	computerScore++;
+	computerScore_span.innerHTML = computerScore;
+	const smallWord = "Computer Won!".fontsize(3).sup();
+	result_p.innerHTML = `${userSelected} beats ${computerSelected}! ${smallWord}`;
 }
-function draw() {
-	console.log("draww");
+function draw(userSelected, computerSelected) {
+	const smallWord = "It's a draw!".fontsize(3).sup();
+	result_p.innerHTML = `${userSelected} and ${computerSelected}. ${smallWord}`;
 }
 function gameUser(userChoice) {
 	const computerChoice = gameComputer();
@@ -51,10 +54,41 @@ function gameUser(userChoice) {
 			draw(userChoice, computerChoice);
 			break;
 	}
+	// if ((userChoice = "rock")) {
+	// 	user_choice.innerHTML = '<i class="fa fa-hand-rock-o fa-3x" aria-hidden="true"></>';
+	// } else if ((userChoice = "paper")) {
+	// 	user_choice.innerHTML = '<i class="fa fa-hand-paper-o fa-3x" aria-hidden="true"></i>';
+	// } else {
+	// 	('<i class="fa fa-hand-scissors-o fa-3x" aria-hidden="true"></>');
+	// }
+	switch (userChoice) {
+		case "rock":
+			user_choice.innerHTML = '<i class="fa fa-hand-rock-o fa-3x" aria-hidden="true"></>';
+			break;
+		case "paper":
+			user_choice.innerHTML = '<i class="fa fa-hand-paper-o fa-3x" aria-hidden="true"></>';
+			break;
+		case "scissor":
+			user_choice.innerHTML = '<i class="fa fa-hand-scissors-o fa-3x" aria-hidden="true"></>';
+			break;
+	}
+	switch (computerChoice) {
+		case "rock":
+			computer_choice.innerHTML = '<i class="fa fa-hand-rock-o fa-3x" aria-hidden="true"></>';
+			break;
+		case "paper":
+			computer_choice.innerHTML = '<i class="fa fa-hand-paper-o fa-3x" aria-hidden="true"></>';
+			break;
+		case "scissor":
+			computer_choice.innerHTML = '<i class="fa fa-hand-scissors-o fa-3x" aria-hidden="true"></>';
+			break;
+	}
+	// userChoice = "rock"
+	// 	? (user_choice.innerHTML = '<i class="fa fa-hand-rock-o fa-3x" aria-hidden="true"></>')
+	// 	: (userChoice = "paper"
+	// 			? (user_choice.innerHTML = '<i class="fa fa-hand-paper-o fa-3x" aria-hidden="true"></>')
+	// 			: (user_choice.innerHTML = '<i class="fa fa-hand-scissors-o fa-3x" aria-hidden="true"></>'));
 }
-// function game(userChoice) {
-// 	console.log("who0000", +userChoice);
-// } //returns NaN -> todo: fix later
 
 //Game functions, Event Listeners
 function main() {
@@ -62,8 +96,6 @@ function main() {
 	paper_div.addEventListener("click", () => gameUser("paper"));
 	scissor_div.addEventListener("click", () => gameUser("scissor"));
 }
-
-//UI change
 
 //Activate the game!
 console.log(main());
