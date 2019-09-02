699b85361edf66288071a4fffa6137b9a719f784
unknown <hiruthstautland@gmail.com>
2019-09-02 19:03:00 +0200

    reduce to turn the array into a single string

--- MovieSearchApp/main.js
+++ MovieSearchApp/main.js
@@ -10,34 +10,26 @@ const resultsSection = document.querySelector("#results");
 form.addEventListener("submit", formSubmitted);
 function formSubmitted(e) {
 	e.preventDefault();
-	console.log("form submitted");
-
 	const searchTerm = input.value;
-	getResults(searchTerm);
+	getResults(searchTerm).then(showResults);
 }
 
 function getResults(searchTerm) {
 	const url = `${API_URL}${searchTerm}&apikey=${API_KEY}`;
-	fetch(url)
-		.then((resp) => resp.json())
-		.then((data) => {
-			showResults(data.Search);
-			console.log(data.Search);
-		})
-		.catch((error) => console.log(error));
+	return fetch(url).then((resp) => resp.json()).then((data) => data.Search).catch((error) => console.log(error));
 }
 function showResults(res) {
-	resultsSection.innerHTML = "";
-	let displayResults = "";
-	res.map((movie) => {
-		displayResults += `<div class="card column m-1">
+	resultsSection.innerHTML = res.reduce((displayResults, movie) => {
+		return (
+			displayResults +
+			`<div class="card column m-1">
             <img class="card-img-top" onerror='this.src="/images/No-Photo-Available.jpg"' src="${movie.Poster}" alt="${movie.Title}">
             <div class="card-body">
               <h5 class="card-title"><b>${movie.Title}</b></h5>
               <p class="card-text">${movie.Year}</p>
               <a href="https://www.imdb.com/title/${movie.imdbID}/?ref_=fn_al_tt_4" target="_blank" class="btn btn-danger">Go somewhere</a>
             </div>
-          </div>`;
-	});
-	resultsSection.innerHTML = displayResults;
+          </div>`
+		);
+	}, "");
 }
